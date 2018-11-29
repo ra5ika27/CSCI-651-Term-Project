26a5e9e2b8897aae2fd26f5a1e2bb91e1bfd0a01
ra5ika27 <31758298+ra5ika27@users.noreply.github.com>
2018-11-29 16:58:49 -0500

    Updated Improved_Main.java

--- Improved_Main.java
+++ Improved_Main.java
@@ -27,21 +27,48 @@ public class Improved_Main {
         Improved_Node root = null;
         try {
 
-            br = new BufferedReader(new FileReader(args[0]));
+            br = new BufferedReader(new FileReader("/Users/rasikathorat/IdeaProjects/FCN/src/CSCi-651-Term-Project/dataset.txt"));
             String c;
             while((c = br.readLine())!=null){
 
-                String[] split = c.split("\\s+");
-                StringBuilder ipAddress = new StringBuilder();
-                int i;
-                for(i=0;i<split[0].length()-3;i++){
-                    ipAddress.append(split[0].charAt(i));
+//                String[] split = c.split("\\s+");
+//                StringBuilder ipAddress = new StringBuilder();
+//                int i;
+//                for(i=0;i<split[0].length()-3;i++){
+//                    ipAddress.append(split[0].charAt(i));
+//                }
+//
+//                int subnet = Integer.parseInt(c.substring(i+1,split[0].length()));
+//                String nextHop = c.substring(20,c.length());
+//                String netPrefix = ipToPrefix(subnet, ipAddress.toString());
+
+                String nextHop = "";
+                String ipAddress = "";
+                String subnet = "";
+                int start = 0;
+                char ch = c.charAt(0);
+
+                int i = 0;
+                while(ch != '/'){
+                    ipAddress += ch;
+                    i ++;
+                    ch = c.charAt(i);
                 }
+                System.out.println(ipAddress);
+                i++;
+                ch = c.charAt(i);
+                while(ch != ' '){
+                    subnet += ch;
+                    i++;
+                    ch = c.charAt(i);
 
-                int subnet = Integer.parseInt(c.substring(i+1,split[0].length()));
-                String nextHop = c.substring(20,c.length());
-                String netPrefix = ipToPrefix(subnet, ipAddress.toString());
 
+                }
+                nextHop = c.substring(i+1,c.length());
+                System.out.println(nextHop);
+                System.out.println(subnet);
+                String netPrefix = ipToPrefix(Integer.parseInt(subnet),ipAddress);
+                System.out.println(netPrefix);
                 if(root == null)
                     root = Improved_Node.insert(null, netPrefix, 0, nextHop);
 
